3e91e2183d4ab05037a193ca5c9b493a1c163b04
liamzdenek <liamzdenek@gmail.com>
2012-12-05 20:00:29 -0500

    generate all the things

--- gen.c
+++ gen.c
@@ -32,38 +32,105 @@
 
 static int genAddress(uint8_t addressOut[40],
                       uint8_t privateKeyHexOut[65],
-                      uint8_t publicKeyBase32Out[53])
+                      uint8_t publicKeyBase32Out[53],
+                      uint8_t privateKey[32])
 {
     struct Address address;
-    uint8_t privateKey[32];
 
-    for (;;) {
-        //randombytes(privateKey, 32);
-        randombytes(privateKey, 32);
-        crypto_scalarmult_curve25519_base(address.key, privateKey);
-        AddressCalc_addressForPublicKey(address.ip6.bytes, address.key);
-        // Brute force for keys until one matches FC00:/8
-        if(
-            address.ip6.bytes[0] == 0xFC// &&
-            //(address.ip6.bytes[15] & 0xF) == (address.ip6.bytes[15] & 0x0F << 4) &&
-            //address.ip6.bytes[14] == address.ip6.bytes[15]
-        )
-        {
-            Hex_encode(privateKeyHexOut, 65, privateKey, 32);
-            Base32_encode(publicKeyBase32Out, 53, address.key, 32);
-            Address_printIp(addressOut, &address);
-            return 0;
-        }
+    //randombytes(privateKey, 32);
+    crypto_scalarmult_curve25519_base(address.key, privateKey);
+    AddressCalc_addressForPublicKey(address.ip6.bytes, address.key);
+    // Brute force for keys until one matches FC00:/8
+    if(
+        address.ip6.bytes[0] == 0xFC// &&
+        //(address.ip6.bytes[15] & 0xF) == (address.ip6.bytes[15] & 0x0F << 4) &&
+        //address.ip6.bytes[14] == address.ip6.bytes[15]
+    )
+    {
+        Hex_encode(privateKeyHexOut, 65, privateKey, 32);
+        Base32_encode(publicKeyBase32Out, 53, address.key, 32);
+        Address_printIp(addressOut, &address);
+        return 1;
     }
+    return 0;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     uint8_t AddressOut[40];
     uint8_t privateKeyHexOut[65];
     uint8_t publicKeyBase32Out[53];
+    uint8_t privateKey[32];
+    int i = 0;
+    int limit = 0;
+    bool has_limit = false;
     
-    genAddress(AddressOut, privateKeyHexOut, publicKeyBase32Out);
+    if(argc == 1)
+    {
+        // run FOREVAR
+        for(i = 0; i < 32; i++)
+        {
+            privateKey[i] = 0x00;
+        }
+    }
     
-    printf("private: %s\n\npublic: %s.k\n\naddr %s\n", privateKeyHexOut, publicKeyBase32Out, AddressOut);
+    if(argc > 1)
+    {
+        // run FOREVAR with a new starting point
+        for(i = 0; i < 32; i++)
+        {
+            privateKey[i] = (argv[1][i*2]-48 << 4) + argv[1][i*2+1]-48;
+        }
+    }
+    if(argc > 2)
+    {
+        // run FOREVAR with a limit so it doesn't actually run forever
+        has_limit = true;
+        limit = atoi(argv[2]);
+    }
+    
+    Hex_encode(privateKeyHexOut, 65, privateKey, 32);
+    //printf("private: %s\n limit:%d\n", privateKeyHexOut, limit);
+    
+    for(;;)
+    {
+        if(has_limit)
+        {
+            limit--;
+            if(limit <= 0)
+            {
+                return 0;
+            }
+        }
+        //randombytes(privateKey, 32);
+        increment_uint8_t(privateKey, 1);
+        Hex_encode(privateKeyHexOut, 65, privateKey, 32);
+        if(genAddress(AddressOut, privateKeyHexOut, publicKeyBase32Out, privateKey))
+        {
+            printf("private: %s\npublic: %s.k\naddr: %s\n\n", privateKeyHexOut, publicKeyBase32Out, AddressOut);
+            //return 0;
+        }
+    }
+}
+
+void increment_uint8_t( uint8_t string[32], int amount )
+{
+    int i = 0;
+    int i2 = 0;
+    for(i2 = 0; i2 < amount; i2++)
+    {
+        for(i = 0; i < 32; i++)
+        {
+            if(string[i] == 0xFF)
+            {
+                string[i] = 0x00;
+            }
+            else
+            {
+                string[i]++;
+                break;
+            }
+        }
+    }
+    return;
 }
